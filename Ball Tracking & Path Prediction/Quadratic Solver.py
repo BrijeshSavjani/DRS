dc66ff3772f5cf0418e5ad74eed5e07390710279
BrijeshSavjani <21848@mynsb.co.uk>
2021-03-13 01:56:53 +0000

    Values Calculated

--- Ball Tracking & Path Prediction/Quadratic Solver.py
+++ Ball Tracking & Path Prediction/Quadratic Solver.py
@@ -1,3 +1,5 @@
+from decimal import *
+import decimal
 class Equation:
     def __init__(self,x,y):
         #Coefficients of the letter. 
@@ -11,12 +13,11 @@ class Equation:
         self.C *= factor
         self.Y *= factor
 def subtract(orignal, subtract):
-    result = orignal
-    result.A -= subtract.A
-    result.B -= subtract.B
-    result.C -= subtract.C
-    result.Y -= subtract.Y
-    return result
+    orignal.A -= subtract.A
+    orignal.B -= subtract.B
+    orignal.C -= subtract.C
+    orignal.Y -= subtract.Y
+    return orignal
 #User Input
 equations_invalid = True
 while equations_invalid:
@@ -31,5 +32,12 @@ while equations_invalid:
         print("Incorrect input")
 r1 = subtract(eq1,eq2)
 r2 = subtract(eq2,eq3)
-r1.multiply((X[0] - X[1])**-1)
-r2.multiply((X[0] - X[1])**-1)
+r1.multiply((float(X[0]) - float(X[1]))**-1)
+r2.multiply((float(X[1]) - float(X[2]))**-1)
+r3 = subtract(r1,r2)
+r3.multiply((r3.A ** -1))
+A = r3.Y
+B =r2.Y - (A * r2.A)
+eq1 = Equation(float(X[0]),float(Y[0]))
+C = eq1.Y - ((eq1.A * A) + (eq1.B * B))
+print(A, B, C)
