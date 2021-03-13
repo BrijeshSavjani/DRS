651f519f310591cd3d2ac615fac304c8a395b504
BrijeshSavjani <21848@mynsb.co.uk>
2021-03-13 17:28:13 +0000

    Add try,except in case 0 contours found in a frame

--- Ball Tracking & Path Prediction/OpenCV (Ball Tracking)/Contour Detection.py
+++ Ball Tracking & Path Prediction/OpenCV (Ball Tracking)/Contour Detection.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-video = cv2.VideoCapture(R"D:\DRS\White Ball.mp4")
+video = cv2.VideoCapture(R"D:\DRS\CropWB.mp4")
 
 lower_bound = np.uint8([186,164,123])
 upper_bound = np.uint8([255,255,255])
@@ -15,15 +15,17 @@ while (video.isOpened()):
     masked = cv2.inRange(resized, lower_bound, upper_bound)
     bitwised = cv2.bitwise_and(resized, resized, mask = masked)    
     reduced = cv2.fastNlMeansDenoising(masked,7,7)
-    
-    contours ,heirachy = cv2.findContours(reduced, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    maximumArea = cv2.contourArea(contours[0])
-    for i in range(0,len(contours)):
-        area = cv2.contourArea(contours[i])
-        if  int(area) > 1 and int(area) < 12:
-            cv2.drawContours(blank,contours,i,(0,255,0),-3)
-            if max([maximumArea,area]) == area:
-                maximumArea = area
+    try:
+        contours ,heirachy = cv2.findContours(reduced, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        maximumArea = cv2.contourArea(contours[0])
+        for i in range(0,len(contours)):
+            area = cv2.contourArea(contours[i])
+            if  int(area) > 1 and int(area) < 12:
+                cv2.drawContours(blank,contours,i,(0,255,0),-3)
+                if max([maximumArea,area]) == area:
+                    maximumArea = area
+    except:
+        print("error")
     cv2.imshow("Output",blank)
     cv2.imshow("bitwise",bitwised)
     cv2.imshow("Video", resized)
